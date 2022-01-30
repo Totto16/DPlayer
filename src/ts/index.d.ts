e271a2f39e3513232eb9954d7f0e56988f3f9d69
Totto16 <tobiausgais@gmail.com>
2022-01-30 17:29:24 +0100

    Add TODO(#31)

--- src/ts/index.d.ts
+++ src/ts/index.d.ts
@@ -1,7 +1,7 @@
 /* declare namespace DPlayerTypes {
-    // TODO use this!
+    // TODO(#31):  use this!
 
-    // TODO remove generic indexable Type!!
+    // TODO:  remove generic indexable Type!!
     // ATTENTION use with cause, since we can't use every string to to that!
     interface StringIndexableObject {
         [index: string]: unknown;
